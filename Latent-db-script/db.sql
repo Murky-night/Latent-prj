7474d18e4ad1b524a644643b364679051c49118f
Tran Dang Khoi <khoi23122005@gmail.com>
2026-04-07 17:12:53 +0700

    fix: add vibe table to store all available vibes  and vibe column to user table

--- Latent-db-script/db.sql
+++ Latent-db-script/db.sql
@@ -12,3 +12,38 @@ CREATE TABLE users (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE prefered_vibe (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,      -- e.g., "Coffee Threads"
+    slug VARCHAR(50) UNIQUE NOT NULL,      -- e.g., "coffee-threads" (Best for URLs and React keys)
+    description TEXT,                      -- Optional context
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO prefered_vibe (name, slug) VALUES 
+('Industrial', 'industrial'),
+('Green-ist', 'green_ist'),
+('Artistic', 'artistic'),
+('Vintage', 'vintage'),
+('Hidden', 'hidden'),
+('Nostalgic', 'nostalgic'),
+('Cinematic', 'cinematic'),
+('Work-friendly', 'work_friendly'),
+('Open-air', 'open_air'),
+('Pet-friendly', 'pet_friendly'),
+('Cozy', 'cozy');
+
+-- 2. Add the array column to the existing users table
+ALTER TABLE users 
+ADD COLUMN preferred_vibes VARCHAR(50)[] DEFAULT '{}';
+
+-- 3. Add a strict database-level constraint to allow a maximum of 3 interests
+ALTER TABLE users
+ADD CONSTRAINT max_three_vibes 
+CHECK (
+    array_length(preferred_vibes, 1) IS NULL 
+    OR 
+    array_length(preferred_vibes, 1) <= 3
+);
